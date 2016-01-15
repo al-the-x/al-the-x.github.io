e2f2180f2f0647da6a8c614fa5bcccf28a6f3603
David Rogers <hello@al-the-x.me>
2016-01-15 16:23:37 -0500

    Hook up `slideout.js` with a simple timed trigger

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+  var menu = new Slideout({
+    menu: document.getElementById('menu'),
+    panel: document.getElementById('panel'),
+  });
+
+  setTimeout(function(){
+    menu.open();
+  }, 500);
+
+  setTimeout(function(){
+    menu.close();
+  }, 3000);
+})();
