7922a73457f6198c5404891db30df0e76d0bb3b0
David Rogers <hello@al-the-x.me>
2016-01-19 14:09:51 -0500

    Add a trigger to open the slideout menu

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-(function(){
-  var menu = new Slideout({
-    menu: document.getElementById('menu'),
-    panel: document.getElementById('panel'),
-    side: 'right',
-  });
+jQuery.noConflict()(function($){
+  var $document = $(this);
 
-  setTimeout(function(){
-    menu.open();
-  }, 500);
-
-  setTimeout(function(){
-    menu.close();
-  }, 3000);
-})();
+  $document
+    .data('menu', new Slideout({
+      menu: document.getElementById('menu'),
+      panel: document.getElementById('panel'),
+      side: 'right',
+    }))
+    .on('toggle.slideout', function(){
+      $(this).data('menu').toggle();
+    })
+    .on('click', 'a[rel="slideout-toggle"]', function(){
+      $document.trigger('toggle.slideout');
+    })
+  ; // END $document
+});
