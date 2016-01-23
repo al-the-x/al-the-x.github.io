d6090aebffbb0361090c87343bed05e24c419e0f
David Rogers <hello@al-the-x.me>
2016-01-23 17:28:17 -0500

    Add result of build...

--- js/all.js
+++ js/all.js
@@ -9901,6 +9901,7 @@ jQuery.noConflict()(function($){
   var $document = $(this);
 
   $document
+    // TODO: Refactor to separate file?
     .data('menu', new Slideout({
       menu: document.getElementById('menu'),
       panel: document.getElementById('panel'),
@@ -9914,19 +9915,37 @@ jQuery.noConflict()(function($){
 
       return false;
     })
+    // END init slideout.js
+
+    // TODO: Refactor to separate file?
+    .on('click', 'summary, .summary', function(){
+      $(this).closest('details, .details').trigger('toggle.details');
+    })
+    .on('open.details', 'details, .details', function(){
+      $(this).attr('open', true);
+    })
+    .on('close.details', 'details, .details', function(){
+      $(this).attr('open', false);
+    })
+    .on('toggle.details', 'details, .details', function(){
+      $(this).trigger( ( $(this).is('[open]') ?
+        'close' : 'open'
+      ) + '.details');
+    })
+    // END <details> <summary> polyfill
+
+    // TODO: Refactor to separate file?
+    .on('click', 'a[rel*="track"]', function(){
+      if (!window.ga) return; // Analytics disabled on `localhost`...
+
+      ga('save', 'event', $.extend({
+        eventCategory: 'link',
+        eventAction: 'click'
+      }, $(this).data));
+    }) // click(a[rel*="track"])
   ; // END $document
 
-  $document.on('click', 'a[rel*="track"]', function(){
-    if (!window.ga) return;
-
-    ga('save', 'event', $.extend({
-      eventCategory: 'link',
-      eventAction: 'click'
-    }, $(this).data));
-  }); // click(a[rel*="track"])
-
   if ( document.location.hash === '#menu' ){
-    console.log('open');
     $document.trigger('toggle.slideout');
   }
 });
