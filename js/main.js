6160c885df06c0f09db56bd8ac97527b2ee74ce4
David Rogers <hello@al-the-x.me>
2016-01-23 17:27:32 -0500

    Define simple polyfill for `<details>` / `<summary>` ...with events!

--- js/main.js
+++ js/main.js
@@ -19,6 +19,23 @@ jQuery.noConflict()(function($){
     // END init slideout.js
 
     // TODO: Refactor to separate file?
+    .on('click', 'summary, .summary', function(){
+      $(this).closest('details, .details').trigger('toggle.details');
+    })
+    .on('open.details', 'details, .details', function(){
+      $(this).attr('open', true);
+    })
+    .on('close.details', 'details, .details', function(){
+      $(this).attr('open', false);
+    })
+    .on('toggle.details', 'details, .details', function(){
+      $(this).trigger( ( $(this).is('[open]') ?
+        'close' : 'open'
+      ) + '.details');
+    })
+    // END <details> <summary> polyfill
+
+    // TODO: Refactor to separate file?
     .on('click', 'a[rel*="track"]', function(){
       if (!window.ga) return; // Analytics disabled on `localhost`...
 
